1d0e2b920ee16f5ab067f08aef08668cab568b68
shumit14 <shumitbhowmick05@gmail.com>
2026-02-13 02:32:20 +0530

    formate changed

--- backend/middleware/uploadResume.js
+++ backend/middleware/uploadResume.js
@@ -5,7 +5,7 @@ const cloudinary = require('../utils/cloudinary')
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'resumes',
+    folder: 'resume',
     resource_type: 'image', 
     allowed_formats: ['pdf']
   }
